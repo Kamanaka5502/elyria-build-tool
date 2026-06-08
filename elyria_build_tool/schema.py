b23eeb58c45a813378fe8f9877e5d8f2275f1194
Samantha Greenwell Revita <samgreenrevita@gmail.com>
2026-06-08 13:31:36 -0600

    Add package source

---
+++ elyria_build_tool/schema.py
@@ -0,0 +1,6 @@
+REQUIRED_FIELDS = [
+    "governed_object", "consequence_description", "authority_requirements",
+    "evidence_requirements", "admit_hold_refuse_matrix", "failure_modes",
+    "receipt_requirements", "replay_requirements", "pilot_corridor_plan",
+    "protected_scope", "commercial_access_boundary", "validation_report"
+]
